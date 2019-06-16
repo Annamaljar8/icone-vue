62333bf3ee50eb301ce902403bd078484dfbea66
Annamaljar8 <41546575+Annamaljar8@users.noreply.github.com>
2019-06-16 14:16:07 +0200

    vue-carousel

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,32 @@
 <template>
-  <div>
-      <p>Home</p>
-  </div>
-</template>
+      <carousel :data="data"></carousel>
+ </template>
 
 <script>
 export default {
-
+    data() {
+      return {
+        data: [
+          '<div class="slide"><img src="src/assets/slide.jpg" class="img"></div>',
+          '<div class="slide"><img src="src/assets/slide1.jpg" class="img"></div>',
+          '<div class="slide"><img src="src/assets/slide2.jpg"class="img"></div>',
+        ],
+      };
+    },
 
 }
 </script>
 
 <style>
+.slide {
+    align-items: center;
+    display: flex;
+    font-size: 1.5rem;
+    justify-content: center;
+  }
+  .img {
+     width: 100%;
+     height: 90vh;
+
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import VueRouter from 'vue-router';
 import App from './App.vue'
 import { routes } from './routes';
+//import VueCarousel from 'vue-carousel';
+import VueCarousel from '@chenfengyuan/vue-carousel';
+
 
 Vue.use(VueRouter);
+Vue.use(VueCarousel);
+
 
 const router = new VueRouter({
     routes,
